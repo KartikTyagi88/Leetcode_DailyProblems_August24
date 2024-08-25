918d6133d033dd8bde946086e9a362a9eaff9130
KartikTyagi88 <kartiktyagi51kt@gmail.com>
2024-08-25 13:03:34 +0530

    Adding the code

---
+++ Aug25.cpp
@@ -0,0 +1,36 @@
+/*
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+ 
+class Solution {
+public:
+
+void solve(TreeNode* root, vector<int>& ans){
+    if(root == NULL) return;
+
+    //Going to the left:
+    solve(root->left, ans);
+    //Going to the right:
+    solve(root->right, ans);
+    
+    // This is the current node:
+    ans.push_back(root->val);
+}
+
+    vector<int> postorderTraversal(TreeNode* root) {
+
+        vector<int> ans;
+
+        solve(root, ans);
+
+        return ans;   
+    }
+};
+*/
